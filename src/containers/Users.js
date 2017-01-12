03fe692ee75f0f175eafe7f28032608538f0adcd
rosstapson <ross.tapson@gmail.com>
2017-01-12 16:05:24 +0200

    admin stuffs

---
+++ src/containers/Users.js
@@ -0,0 +1,19 @@
+
+import React, {Component, /*PropTypes*/} from 'react'
+//import UserList from './components/UserList'
+
+//import {browserHistory} from 'react-router';
+
+export default class Users extends Component {
+
+  render() {
+     return (
+      <div>
+        <div className="asset-title">
+          Users
+        </div>
+      </div>
+     )
+  }
+
+}
