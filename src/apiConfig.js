2ab54eaac11f47f3de15bd47d5fe23ad0b8df9eb
rosstapson <ross.tapson@gmail.com>
2017-09-22 15:42:20 +0200

    logging

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,6 +2,7 @@ let apiHost;
 
 if (process.env.NODE_ENV === 'development') {
     apiHost = "http://localhost:3000";
+    console.log("process.env.NODE_ENV is 'development'");
 }
 else {
     apiHost = "https://seekerdnasecure.co.za:3002";
